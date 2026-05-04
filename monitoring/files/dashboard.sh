c3803fdf1cfa22b1834809da99cecb3b9ac57e0d
Aaro Hinkkanen <aaro.hinkkanen@gmail.com>
2026-05-04 11:23:30 +0300

    Added one line to dashboard.sh

--- monitoring/files/dashboard.sh
+++ monitoring/files/dashboard.sh
@@ -3,15 +3,15 @@
 LOG="/var/log/monitoring/metrics.log"
 
 clear
-echo"====================="
-echo"| SERVER MONITORING |"
-echo"====================="
-
+echo "====================="
+echo "| SERVER MONITORING |"
+echo "====================="
+echo "Viimeisin tila:"
 # Viimeinen rivi logista
 tail -n 1 $LOG
 
-echo"========================="
-echo"|Viimeiset 5 tapahtumaa:|"
-echo"|=======================|"
+echo "========================="
+echo "|Viimeiset 5 tapahtumaa:|"
+echo "|=======================|"
 tail -n 5 $LOG
-echo"========================="
+echo "========================="
