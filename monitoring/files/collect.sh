97298d6695d2f59ed0e8fb1c3e10ff737db4b5d3
Minttu Hillebrandt <minttuhillebrandt@gmail.com>
2026-05-04 11:11:06 +0300

    Add " in collect.sh

--- monitoring/files/collect.sh
+++ monitoring/files/collect.sh
@@ -13,4 +13,4 @@ RAM=$(free | grep Mem | awk '{printf "%.0f", $3/$2 * 100}')
 DISK=$(df -h / | awk 'NR==2 {print $5}' | cut -d'%' -f1)
 
 # Tallenetaan lokiin
-echo "$DATE | CPU: $CPU% | RAM: $RAM% | DISK: $DISK% >> $LOG
+echo "$DATE | CPU: $CPU% | RAM: $RAM% | DISK: $DISK%" >> $LOG
